d807133df2f943f396846befa45e4314e6e4cf7e
Jose Julian Vargas Cubillas <84953819+JoseVargasCubillas@users.noreply.github.com>
2024-04-17 23:57:00 -0600

    Update axios.jsx

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+    baseURL: `https://lobster-app-tjkht.ondigitalocean.app/api`,
 })
 
 export default clienteAxios;
